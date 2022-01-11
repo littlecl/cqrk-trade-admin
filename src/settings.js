2cd98f43ad7fc7ba08dbf1b07162671ed9c447e7
lzr <lzr@qq.com>
2022-01-11 16:32:10 +0800

    二手市场后台初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '重人科二手市场后台管理',//页面标题
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,//nav是否固定
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false //侧边栏logo区域
+}
